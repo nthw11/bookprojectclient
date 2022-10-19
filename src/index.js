79c5b3007548e151ab4e11c1eafbb0d6a927084b
Nathaniel <nathaniel.wallace@gmail.com>
2022-10-19 15:57:24 -0400

    initial commit, set up react app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import store from "./store"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
