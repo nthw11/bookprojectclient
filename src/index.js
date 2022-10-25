f0f9e09503919b96c5f43b018a72fcaded2a3bde
Nathaniel <nathaniel.wallace@gmail.com>
2022-10-25 09:14:06 -0400

    Revert "some components are on the page"
    This reverts commit ae99aca1cb5347f1ff34e492c2b73cb5e90100ff.

--- src/index.js
+++ src/index.js
@@ -2,30 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import store from "./store"
+import store from "./store"
 import './index.css';
 import App from './App';
-import Home from './components/Pages/Home';
-import UserMainPage from './components/Pages/UserMainPage';
-import NotFound from './components/Pages/NotFound';
-import BookDetail from './components/Pages/BookDetail';
-import SearchPage from './components/Pages/SearchPage';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Provider store={store}>
-
-    <BrowserRouter>
-    <Routes>
-    {/* <Route path="/" element={<App />} /> */}
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<UserMainPage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/book/:bookId" element={<BookDetail />} />
-      <Route page="*" element={<NotFound />} />
-    </Routes>
-    </BrowserRouter>
-
-  // </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
