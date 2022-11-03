f8c3c4581e66248283c9de4ef6f0e67ac77e94a3
Nathaniel <nathaniel.wallace@gmail.com>
2022-11-03 17:42:07 -0400

    can add new user still need to redirect back home, plus need user id

--- src/components/Pages/NewUser.js
+++ src/components/Pages/NewUser.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 import {useForm} from 'react-hook-form'
 import Header from '../Blocks/Header'
 import styled from 'styled-components'
@@ -11,29 +12,54 @@ const StyledNewUserForm = styled.div`
 `
 
 const NewUser = () => {
+  const navigate = useNavigate()
   const [newUser, setNewUser] = useState('')
   const { register, handleSubmit, watch, formState: {errors} } = useForm()
 
   const createNewUserHandler = async (e) =>{
     e.preventDefault()
-    // console.log(e)
+    console.log(e)
   }
-  const onSubmit = data => console.log(data)
+  const onSubmit = async data => {
+    const {username, firstname, lastname, email, phone, avatarUrl, password} = data
+    const config = {
+      method: 'post',
+      data: {
+        username,
+        firstname,
+        lastname,
+        email,
+        phone,
+        avatarUrl,
+        password
+      },
+      url: `${API}/user`
+    }
+    await axios(config).then((userData) => {
+      console.log(userData)
+    }).then(response => {
+      if(response.status === 200){
+        return navigate("/user")
+      }
+    })
+
+  }
 
   return (
     <div>
       <Header />
       <h2>New User Page</h2>
       <StyledNewUserForm>
+      {/* <form onSubmit={createNewUserHandler}> */}
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Username</label>
         <input type="text" placeholder='username' {...register('username', {required: true, minLength: 4, maxLength: 20})} />
         
         <label>First Name</label>
-        <input type="text" placeholder='First Name' {...register("firstName", {required: true})} />
+        <input type="text" placeholder='First Name' {...register("firstname", {required: true})} />
         
         <label>Last Name</label>
-        <input type="text" placeholder='Last Name' {...register('lastName', {required: true})} />
+        <input type="text" placeholder='Last Name' {...register('lastname', {required: true})} />
         
         <label>Email</label>
         <input type="email" placeholder='Email Address' {...register('email', {required: true}) } />
